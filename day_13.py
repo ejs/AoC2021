e2117ce4c51534a6413bf1a325d03874387a1d16
Edward Saxton <eddsax@googlemail.com>
2021-12-13 13:25:32 +0000

    Extract helper function to simplify folding code a lot

--- day_13.py
+++ day_13.py
@@ -1,28 +1,32 @@
-code = open("input_13")
+def fold(point, line):
+    return 2*line-point if point > line else point
 
+
+code = (l.strip() for l in open("input_13"))
+
+# fill starting dots
 paper = set()
-for line in iter(code.readline, "\n"):
-    x, _, y = line.strip().partition(',')
+for line in code:
+    if not line:
+        break
+    x, _, y = line.partition(',')
     paper.add((int(x), int(y)))
-print(len(paper))
 
 
-for fold in code:
+# follow fold instructions
+for instuction in code:
 
-    instruction, _, line = fold.strip().partition("=")
-    if instruction[-1] == "x":
+    # this puts a virtual fold far off the paper on the other side
+    # to simplify the later folding logic
+    description, _, line = instuction.partition("=")
+    if description[-1] == "x":
         x_line, y_line = int(line), 2000
-    elif instruction[-1] == "y":
+    elif description[-1] == "y":
         x_line, y_line = 2000, int(line)
 
-    folded = set()
-    for x, y in list(paper):
-        ny = 2*y_line-y if y > y_line else y
-        nx = 2*x_line-x if x > x_line else x
-        folded.add((nx, ny))
-    paper = folded
-
+    paper = {(fold(x, x_line), fold(y, y_line)) for x, y in list(paper)}
     print(len(paper))
 
+# display final state
 for y in range(max(y for (x, y) in paper)+1):
     print("".join("•" if (x, y) in paper else " " for x in range(max(x for (x, y) in paper)+1)))
