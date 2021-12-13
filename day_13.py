ee6007167671843820b46542d5a089e862c9c229
Edward Saxton <eddsax@googlemail.com>
2021-12-13 12:35:39 +0000

    Simplify folding code slightly

--- day_13.py
+++ day_13.py
@@ -8,22 +8,21 @@ print(len(paper))
 
 
 for fold in code:
-    folded = set()
 
     instruction, _, line = fold.strip().partition("=")
     if instruction[-1] == "x":
-        y_line = 2000
-        x_line = int(line)
+        x_line, y_line = int(line), 2000
     elif instruction[-1] == "y":
-        x_line = 2000
-        y_line = int(line)
+        x_line, y_line = 2000, int(line)
 
+    folded = set()
     for x, y in list(paper):
         ny = 2*y_line-y if y > y_line else y
         nx = 2*x_line-x if x > x_line else x
         folded.add((nx, ny))
     paper = folded
+
     print(len(paper))
 
 for y in range(max(y for (x, y) in paper)+1):
-    print("".join("X" if (x, y) in paper else " " for x in range(max(x for (x, y) in paper)+1)))
+    print("".join("•" if (x, y) in paper else " " for x in range(max(x for (x, y) in paper)+1)))
